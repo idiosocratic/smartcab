8b94dd558f8bd934b2739edcf1aa479e32181ddb
idiosocratic <david.baugher@idiosocratic.com>
2016-03-13 16:44:01 -0500

    Added parser/converter from env.sense to state/action dict key

--- smartcab/agent.py
+++ smartcab/agent.py
@@ -23,6 +23,34 @@ class LearningAgent(Agent):
         deadline = self.env.get_deadline(self)
 
         # TODO: Update state
+        state_to_binary =''
+        
+        # from the sense function create the corresponding dict key
+        # construct state to binary
+        if self.next_waypoint == 'forward':
+          state_to_binary += '11'
+        elif self.next_waypoint == 'left':
+          state_to_binary += '10'
+        elif self.next_waypoint == 'right':
+          state_to_binary += '01'
+        elif self.next_waypoint == 'None':
+          state_to_binary += '00'
+          
+        
+        if inputs.light == 'green':
+          state_to_binary += '1'
+        elif inputs.light == 'red':
+          state_to_binary += '0'
+          
+        if inputs.oncoming_now == 'left':
+          state_to_binary += '10'
+        elif inputs.left_now == 'forward':
+          state_to_binary += '01'
+        elif inputs.oncoming_now == 'forward':
+          state_to_binary += '11'
+        else:
+          state_to_binary += '00'
+          
         
         # TODO: Select action according to your policy
         action = None
@@ -46,19 +74,22 @@ state_action_dictionary = {'11111':{'right':0,'left':0,'forward':0,'None':0}, '1
                            '01010':{'right':0,'left':0,'forward':0,'None':0}, '01000':{'right':0,'left':0,'forward':0,'None':0},
                            '01111':{'right':0,'left':0,'forward':0,'None':0}, '01110':{'right':0,'left':0,'forward':0,'None':0},
                            '01100':{'right':0,'left':0,'forward':0,'None':0}, '11011':{'right':0,'left':0,'forward':0,'None':0},
-                           '11010':{'right':0,'left':0,'forward':0,'None':0}, '11000':{'right':0,'left':0,'forward':0,'None':0}}
+                           '11010':{'right':0,'left':0,'forward':0,'None':0}, '11000':{'right':0,'left':0,'forward':0,'None':0},
+                           '11101':{'right':0,'left':0,'forward':0,'None':0}, '10101':{'right':0,'left':0,'forward':0,'None':0},
+                           '10001':{'right':0,'left':0,'forward':0,'None':0}, '00001':{'right':0,'left':0,'forward':0,'None':0},
+                           '00101':{'right':0,'left':0,'forward':0,'None':0}, '01101':{'right':0,'left':0,'forward':0,'None':0},
+                           '11001':{'right':0,'left':0,'forward':0,'None':0}, '01001':{'right':0,'left':0,'forward':0,'None':0}}
                            
-state_count_dictionary = {'11111':0, '11110':0,
-                          '11100':0, '10111':0,
-                          '10110':0, '10100':0,
-                          '10011':0, '10010':0,
-                          '10000':0, '00011':0,
-                          '00010':0, '00000':0,
-                          '00111':0, '00110':0,
-                          '00100':0, '01011':0,
-                          '01010':0, '01000':0,
-                          '01111':0, '01110':0,
-                          '01100':0, '11011':0,
+state_count_dictionary = {'11111':0, '11110':0, '11101':0,
+                          '11100':0, '10111':0, '10101':0,
+                          '10110':0, '10100':0, '10001':0,
+                          '10011':0, '10010':0, '10000':0,
+                          '00001':0, '00011':0, '00010':0,
+                          '00000':0, '00111':0, '00110':0,
+                          '00101':0, '00100':0, '01011':0,
+                          '01010':0, '01000':0, '01001':0,
+                          '01111':0, '01110':0, '01101':0,
+                          '01100':0, '11011':0, '11001':0,
                           '11010':0, '11000':0}                           
 
 
